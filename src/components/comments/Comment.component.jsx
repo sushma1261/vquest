e87d621b917811e66fe07f38716dea52464b69a7
sushma1261 <39758057+sushma1261@users.noreply.github.com>
2020-01-24 09:16:40 +0530

    Commit1

--- src/components/comments/Comment.component.jsx
+++ src/components/comments/Comment.component.jsx
@@ -1,48 +1,24 @@
 import React from 'react';
-import { Segment, Grid, Image, Button, Icon, Label } from 'semantic-ui-react';
-
+import { Grid, Image } from 'semantic-ui-react';
+import download from '../../assests/download.png';
 class Comment extends React.Component {
     render() {
-        return (
-            <div>
-                <Segment.Group style={{backgroundColor: "black"}} >
-                    <Segment padded style={{backgroundColor: "#b5e6e1"}}>
-                        <Grid>
-                            {this.props.number}
+        return(
+            <div style = {{textAlign: "justify"}}>
+                <Grid>
+                            {/* {this.props.number} */}
                             <Grid.Column width = {2}>
-                            <Image size = "mini"  circular src = {this.props.imageurl}/>
+                            <Image size = "mini"  circular src = {download}/>
         <span>{this.props.username}</span>
                             </Grid.Column>
-                            <Grid.Column width = {13} className = "answerText">
-                                <p style = {{fontFamily : "Gregoria", textAlign : "justify", fontSize : "20px"}} >{this.props.answer}</p>
+                            <Grid.Column width = {13}>
+        <p style = {{fontFamily : "Gregoria", textAlign : "justify", fontSize : "16px"}}>{this.props.comment}</p>
                             </Grid.Column>
                             
-                           
-                        </Grid>
-                        <br />
-                        <div style = {{position : "absolute",right: "10px"}}>
-                        {/* <Button as='div' labelPosition='right' > */}
-                                {/* <Button color='red'> */}
-                                    {/* <Icon name='heart' /> */}
-                                {/* </Button> */}
-                                {/* <Label as='a' basic color='red' pointing='left'> */}
-                                    {/* {this.props.likes} */}
-                                {/* </Label> */}
-                            {/* </Button> */}
-                        {/* </div> */}
-                        
-                        {/* <span style = {{position : "absolute",left : "10px"}}> */}
-                            {/* <Button primary>Add a comment</Button> */}
-                        {/* </span> */}
-                            {/* <br /> */}
-                            {/* <br /> */}
-                    </Segment>
-                </Segment.Group>
-                <br />
 
+                        </Grid>
             </div>
         )
     }
 }
-
 export default Comment;
